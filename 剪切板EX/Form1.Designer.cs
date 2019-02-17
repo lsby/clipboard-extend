f308f0c428e4f5e191aa546dd79d990ff35bf32c
lsby <2451759073@qq.com>
2019-02-18 01:28:46 +0800

    base

---
+++ 剪切板EX/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace 剪切板EX
+{
+    partial class 主窗口
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.控件_sp = new System.Windows.Forms.SplitContainer();
+            this.控件_左边 = new System.Windows.Forms.FlowLayoutPanel();
+            this.控件_右边 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.控件_sp)).BeginInit();
+            this.控件_sp.Panel1.SuspendLayout();
+            this.控件_sp.Panel2.SuspendLayout();
+            this.控件_sp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // 控件_sp
+            // 
+            this.控件_sp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.控件_sp.Location = new System.Drawing.Point(0, 0);
+            this.控件_sp.Name = "控件_sp";
+            // 
+            // 控件_sp.Panel1
+            // 
+            this.控件_sp.Panel1.Controls.Add(this.控件_左边);
+            // 
+            // 控件_sp.Panel2
+            // 
+            this.控件_sp.Panel2.Controls.Add(this.控件_右边);
+            this.控件_sp.Size = new System.Drawing.Size(870, 105);
+            this.控件_sp.SplitterDistance = 458;
+            this.控件_sp.TabIndex = 0;
+            // 
+            // 控件_左边
+            // 
+            this.控件_左边.AutoScroll = true;
+            this.控件_左边.AutoSize = true;
+            this.控件_左边.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.控件_左边.Location = new System.Drawing.Point(0, 0);
+            this.控件_左边.Margin = new System.Windows.Forms.Padding(0);
+            this.控件_左边.Name = "控件_左边";
+            this.控件_左边.Size = new System.Drawing.Size(458, 105);
+            this.控件_左边.TabIndex = 0;
+            // 
+            // 控件_右边
+            // 
+            this.控件_右边.AutoScroll = true;
+            this.控件_右边.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.控件_右边.Location = new System.Drawing.Point(0, 0);
+            this.控件_右边.Name = "控件_右边";
+            this.控件_右边.Size = new System.Drawing.Size(408, 105);
+            this.控件_右边.TabIndex = 0;
+            // 
+            // 主窗口
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 105);
+            this.Controls.Add(this.控件_sp);
+            this.Name = "主窗口";
+            this.Text = "ClipboardEx";
+            this.控件_sp.Panel1.ResumeLayout(false);
+            this.控件_sp.Panel1.PerformLayout();
+            this.控件_sp.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.控件_sp)).EndInit();
+            this.控件_sp.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer 控件_sp;
+        private System.Windows.Forms.FlowLayoutPanel 控件_左边;
+        private System.Windows.Forms.FlowLayoutPanel 控件_右边;
+    }
+}
+
---
+++ 剪切板EX/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 剪切板EX
+{
+    public partial class 主窗口 : Form
+    {
+        private int 鼠标y坐标;
+        private int 鼠标滚轮参数;
+        private int 剪切板最大数量 = 50;
+        private int 按钮高度 = 60;
+        private int 窗口高度 = 200;
+
+        public 主窗口()
+        {
+            InitializeComponent();
+
+            设置鼠标钩子();
+            设置定时器();
+            设置监听剪切板();
+            显示();
+        }
+        ~主窗口()
+        {
+            设置取消监听剪切板();
+        }
+
+        private void 设置取消监听剪切板()
+        {
+            RemoveClipboardFormatListener(this.Handle);
+        }
+
+        private void 设置监听剪切板()
+        {
+            AddClipboardFormatListener(this.Handle);
+        }
+
+        private void 设置剪切板内容(String str)
+        {
+            Clipboard.SetDataObject(str, true);
+        }
+
+        void 设置鼠标钩子()
+        {
+            var hook = new MouseKeyHook();
+            hook.OnMouseActivity += 回调_鼠标;
+            hook.Start();
+        }
+
+        void 设置定时器()
+        {
+            Timer t = new Timer();
+            t.Interval = 100;
+            t.Tick += new EventHandler(回调_定时器);
+            t.Enabled = true;
+        }
+
+        void 显示()
+        {
+            Visible = true;
+
+            this.TopMost = true;
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = 窗口高度;
+            Location = new Point(0, 0);
+            控件_sp.SplitterDistance = this.Width / 2;
+        }
+
+        void 隐藏()
+        {
+            Visible = false;
+        }
+
+        List<Button> 左边按钮 = new List<Button>();
+        List<Button> 右边按钮 = new List<Button>();
+
+        void 添加左边按钮(String str)
+        {
+            var button = new Button();
+            button.Text = str;
+            button.Width = 控件_左边.Width - 2;
+            button.Height = 按钮高度;
+            button.Margin = new Padding(0);
+            button.Click += 回调_按钮点击;
+            button.TextAlign = ContentAlignment.TopLeft;
+
+            for (var i = 0; i < 左边按钮.Count; i++)
+                if (左边按钮[i].Text == str)
+                    左边按钮.RemoveAt(i);
+
+            左边按钮.Insert(0, button);
+            while (左边按钮.Count > 剪切板最大数量)
+                左边按钮.RemoveAt(左边按钮.Count - 1);
+
+            渲染左边();
+        }
+
+        private void 回调_按钮点击(object sender, EventArgs e)
+        {
+            设置剪切板内容(((Button)sender).Text);
+            隐藏();
+        }
+
+        void 渲染左边()
+        {
+            if (控件_左边.Controls.Count == 0)
+            {
+                渲染左边_添加(0);
+                return;
+            }
+            while (控件_左边.Controls[控件_左边.Controls.Count - 1].Text != 左边按钮[左边按钮.Count - 1].Text)
+                控件_左边.Controls.RemoveAt(控件_左边.Controls.Count - 1);
+            渲染左边_添加(0);
+        }
+        void 渲染左边_添加(int start)
+        {
+            for (var i = start; i < 左边按钮.Count; i++)
+                控件_左边.Controls.Add(左边按钮[i]);
+        }
+
+        void 回调_定时器(object sender, EventArgs e)
+        {
+            if (!Bounds.Contains(MousePosition) && MouseButtons == MouseButtons.Left)
+                隐藏();
+            else if (鼠标y坐标 <= 0 && 鼠标滚轮参数 < 0)
+                显示();
+            else if (鼠标y坐标 <= 0 && 鼠标滚轮参数 > 0)
+                隐藏();
+        }
+
+        void 回调_鼠标(object sender, MouseEventArgs e)
+        {
+            鼠标y坐标 = e.Y;
+            鼠标滚轮参数 = e.Delta;
+        }
+
+        void 回调_剪切板()
+        {
+            添加左边按钮(Clipboard.GetText());
+        }
+
+        //===================================================剪切板监听
+        [DllImport("user32.dll")]
+        public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+        [DllImport("user32.dll")]
+        public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+        private static int WM_CLIPBOARDUPDATE = 0x031D;
+
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_CLIPBOARDUPDATE)
+                回调_剪切板();
+            else
+                base.DefWndProc(ref m);
+        }
+    }
+}
